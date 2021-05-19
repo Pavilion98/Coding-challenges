6e646b6872a1c3ba219f5599bd2d54bcbee32328
wassim <wassim.alharakeh@mubs.edu.lb>
2021-05-19 05:13:44 +0300

    solved challenge-8

---
+++ question-8.js
@@ -0,0 +1,27 @@
+
+var usernames = ['Wassim', 'Wael', 'Wanda', 'Saleem', 'Salim', 'Salih', 'Sameer'];
+
+
+const autoComplete = (value) => {
+    var output = [];
+  
+    for (i = 0; i < usernames.length; i++) {
+      if ( value === usernames[i].toLowerCase().slice(0, value.length)) {
+        output.push(usernames[i]);
+      }
+      return usernames.filter( e => e.toLowerCase().includes(value.toLowerCase()));
+    }
+  
+    return output;
+  }
+
+
+
+console.log( autoComplete('sal')); //must return [‘Saleem’, ‘Salim’, ‘Salih’]
+console.log( autoComplete('sali')); //must return [‘Salim’, ‘Salih’]
+console.log( autoComplete('ee')); //must return ['Saleem', 'Sameer']
+console.log( autoComplete('wa')); //must return ['Wassim', 'Wael', 'Wanda']
+console.log( autoComplete('was')); //must return ['Wassim']
+
+
+
